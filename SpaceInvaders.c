61ea9d0cbc7de584286f3f127bec9714963e4e5d
Ahmed Mahmoud Sanad <sanada927@gmail.com>
2024-05-18 02:23:24 +0300

    Update SpaceInvaders.c

--- SpaceInvaders.c
+++ SpaceInvaders.c
@@ -54,11 +54,14 @@ long StartCritical (void);    // previous I bit, disable interrupts
 void EndCritical(long sr);    // restore I bit to previous value
 void WaitForInterrupt(void);  // low power mode
 void EdgeCounter_Init(void);
+void initializeSnake(Snake *snake, int x, int y);
+void moveSnake(Snake *snake);
 bool checkFood(Snake *snake, Food *food);
 void generateNewFood(Food *food);
 void initializeFood(Food *food);
-void PortF_Init(void)
-
+void PortF_Init(void);
+void Timer2_Init(unsigned long period);
+bool checkCollision(Snake *snake, int newX, int newY);
 
 
 
@@ -215,3 +218,48 @@ void generateNewFood(Food *food) {
     food->y = Ytemp;
 }
 
+// You can use this timer only if you learn how it works
+void Timer2_Init(unsigned long period){
+  unsigned long volatile delay;
+  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
+  delay = SYSCTL_RCGCTIMER_R;	
+  TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
+  TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER2_TAILR_R = period-1;    // 4) reload value
+  TIMER2_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
+  TIMER2_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|0x80000000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 39, interrupt number 23
+  NVIC_EN0_R = 1<<23;           // 9) enable IRQ 23 in NVIC
+  TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
+}
+void Timer2A_Handler(void){ 
+  TIMER2_ICR_R = 0x00000001;   // acknowledge timer2A timeout
+	 moveSnake(&snake);
+	if (checkFood(&snake, &food)) 
+		{
+			if(outroWaitingFlag==0){
+			++score;
+			}
+     generateNewFood(&food);
+		 snake.length++; //increase the snake length
+    }
+}
+bool checkCollision(Snake *snake, int newX, int newY) {
+    // Check wall collision
+    if (newX < 0 || newX >= WIDTH || newY < 0 || newY >= HEIGHT) {
+        return true;
+    }
+    
+    // Check self collision
+    for ( i = 1; i < snake->length; ++i) {
+        if (snake->x[i] == newX && snake->y[i] == newY) {
+            return true;
+        }
+    }
+    
+    return false;
+}
